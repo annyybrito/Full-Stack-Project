f02dbbdf53933b75cd7e99b23e09088a1b52d216
Anny Brito <anny.brito@animaeducacao.com.br>
2023-12-11 19:06:29 -0300

    finalização do projeto

--- src/script.js
+++ src/script.js
@@ -1,3 +1,4 @@
+
 document.addEventListener('DOMContentLoaded', function () {
   const openModalButton = document.getElementById('new');
   const modalContainer = document.querySelector('.modal-container');
@@ -7,6 +8,8 @@ document.addEventListener('DOMContentLoaded', function () {
 
   openModalButton.addEventListener('click', openModal);
   saveButton.addEventListener('click', saveData);
+
+  let isEditing = false;
 
   function openModal() {
     modalContainer.classList.add('active');
@@ -25,30 +28,21 @@ document.addEventListener('DOMContentLoaded', function () {
   function saveData(event) {
     event.preventDefault();
     const formData = {
+      id: document.getElementById('estudanteId').value,
       nome: document.getElementById('m-nome').value,
       dataDeNascimento: document.getElementById('m-dataDeNascimento').value,
       nomedamae: document.getElementById('m-nomedamae').value,
       periodoDeIngresso: document.getElementById('periodoDeIngresso').value,
     };
 
-
     if (formData.nome && formData.dataDeNascimento && formData.nomedamae && formData.periodoDeIngresso) {
-      // Criação de uma nova linha na tabela
-      const newRow = document.createElement('tr');
-      newRow.innerHTML = `
-        <td>${formData.nome}</td>
-        <td>${formData.dataDeNascimento}</td>
-        <td>${formData.nomedamae}</td>
-        <td>${formData.periodoDeIngresso}</td>
-        <td class="acao"><button onclick="editRow(this)"><i class='bx bxs-edit'></i></button></td>
-        <td class="acao"><button onclick="deleteRow(this)"><i class='bx bxs-trash'></i></button></td>
-      `;
-
-      // Adiciona a nova linha à tabela
-      tbody.appendChild(newRow);
-
-      sendDataToBackend(formData);
-
+      if (isEditing) {
+        updateDataOnBackend(formData);
+      } else {
+        sendDataToBackend(formData);
+      }
+      
+      isEditing = false;
       closeModal();
     } else {
       alert('Preencha todos os campos antes de salvar.');
@@ -56,41 +50,130 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
   function sendDataToBackend(data) {
-     fetch('https://localhost:7275/api/estudantes', {
+    fetch('https://localhost:7275/api/estudantes', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
       },
-      body:JSON.stringify(data),
+      body: JSON.stringify(data),
     })
+
       .then(response => {
         if (!response.ok) {
           throw new Error('Erro ao enviar os dados para o back-end.');
         }
         console.log('Dados enviados com sucesso!');
+        appendRow(data.id);
       })
       .catch(error => {
         console.error(error.message);
-      }); 
+      });
+  }
+  
+  getDataFromBackend ()
+  function getDataFromBackend() {
+    fetch('https://localhost:7275/api/estudantes', {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    })
+
+      .then(async response => {
+        
+        if (!response.ok) {
+          throw new Error('Erro ao receber dados do back-end.');
+        }
+        appendRow(await response.json());
+      })
+      .catch(error => {
+        console.error(error.message);
+      });
   }
 
-  window.editRow = function (button) {
+  function updateDataOnBackend(data) {
+    fetch(`https://localhost:7275/api/estudantes/${data.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao enviar os dados atualizados para o back-end.');
+        }
+        console.log('Dados atualizados com sucesso!');
+        getDataFromBackend();
+      })
+      .catch(error => {
+        console.error(error.message);
+      });
+  }
+
+  window.editRow = function (button, id) {
     const row = button.closest('tr');
     const cells = row.getElementsByTagName('td');
     document.getElementById('m-nome').value = cells[0].innerText;
     document.getElementById('m-dataDeNascimento').value = cells[1].innerText;
     document.getElementById('m-nomedamae').value = cells[2].innerText;
     document.getElementById('periodoDeIngresso').value = cells[3].innerText;
-
-    // Remove a linha editada da tabela
-    tbody.removeChild(row);
-
-    // Abre o modal para edição
+    document.getElementById('estudanteId').value = id;
+    isEditing = true;
     openModal();
   };
 
-  window.deleteRow = function (button) {
+  window.deleteRow = function (button, id) {
     const row = button.closest('tr');
-    tbody.removeChild(row);
+    const cells = row.getElementsByTagName('td');
+    const idToDelete = cells[4].innerText;
+    console.log(id);
+
+    fetch('https://localhost:7275/api/estudantes')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Erro ao obter a lista de estudantes');
+        }
+        return response.json();
+      })
+      .then(estudantes => {
+        deleteEstudante(idToDelete, estudantes);
+      })
+      .catch(error => {
+        console.error('Erro ao obter a lista de estudantes:', error.message);
+      });
   };
+
+  window.deleteEstudante = function (estudantesID) {
+    fetch(`https://localhost:7275/api/estudantes/${estudantesID}`, {
+      method: 'DELETE',
+    })
+      .then(async response => {
+        if (!response.ok) {
+          console.error('Erro ao excluir o estudante do back-end:', response.status, response.statusText);
+          throw new Error('Erro ao excluir o estudante do back-end.');
+        }
+         getDataFromBackend();
+      })
+      .catch(error => {
+        console.error(error.message);
+      });
+  }
+
+  function appendRow(estudantes) { 
+    tbody.innerHTML = '';
+    estudantes.forEach(estudante => {
+      const newRow = document.createElement('tr');
+      newRow.innerHTML = `
+        <td>${estudante.nome}</td>
+        <td>${estudante.dataDeNascimento.split('T')[0]}</td>
+        <td>${estudante.nomeDaMae}</td>
+        <td>${estudante.periodoDeIngresso}</td>
+        <td class="acao"><button onclick="editRow(this,${estudante.id} )"><i class='bx bxs-edit'></i></button></td>
+        <td class="acao"><button onclick="deleteEstudante(${estudante.id})"><i class='bx bxs-trash'></i></button></td>
+      `;
+      tbody.appendChild(newRow);
+    })
+    
+  }
 });
